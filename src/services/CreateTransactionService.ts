5a8acf7bb8bca8e087ff776c28c8642a1ba2e030
sir-teagus <teagus@gmail.com>
2020-11-07 02:00:14 -0300

    Include transaction type validation

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,10 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Invalid transaction type.');
+    }
+
     const { total } = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && value > total) {
